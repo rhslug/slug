0d0318398b28e66acc335e9961e049a75d67b558
Aca7 <hoxz12012@naver.com>
2015-06-17 22:32:53 +0900

    ??

---
+++ Assets/Scripts/click.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class click : MonoBehaviour {
+
+	LineRenderer line;
+	CardboardHead head;
+	// Use this for initialization
+	void Start () {
+		Cardboard.SDK.TapIsTrigger = true;
+		head = Camera.main.GetComponent<StereoController>().Head;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Cardboard.SDK.CardboardTriggered) {
+
+		}
+	}
+
+}
