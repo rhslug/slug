7312272021209b1f89c7e6a16462788724551cf1
Jiwon Choi <1890mah@gmail.com>
2015-06-24 21:45:01 +0900

    360 spawn

--- Assets/makeSlug.cs
+++ Assets/makeSlug.cs
@@ -10,7 +10,16 @@ public class makeSlug : MonoBehaviour {
 	}
 
 	void spawn () {
-		GameObject s = Instantiate(Slug, new Vector3(Random.Range(-35,35), 15, Random.Range(-35,35)), Quaternion.identity) as GameObject;
+		float r = Random.Range(0F, 4F);
+		GameObject s;
+		if(r >= 0 && r < 1)
+			s = Instantiate(Slug, new Vector3(35, 3, -35+r*70), Quaternion.identity) as GameObject;
+		if(r >= 1 && r < 2)
+			s = Instantiate(Slug, new Vector3(-35, 3, -35+(r-1)*70), Quaternion.identity) as GameObject;
+		if(r >= 2 && r < 3)
+			s = Instantiate(Slug, new Vector3(-35+(r-2)*70, 3, 35), Quaternion.identity) as GameObject;
+		if(r >= 3 && r <= 4)
+			s = Instantiate(Slug, new Vector3(-35+(r-3)*70, 3, -35), Quaternion.identity) as GameObject;
 	}
 	
 	// Update is called once per frame
