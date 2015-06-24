386ec324f92b1a6aec04b38f3822fec542404114
Jiwon Choi <1890mah@gmail.com>
2015-06-24 21:27:14 +0900

    spawn

---
+++ Assets/makeSlug.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class makeSlug : MonoBehaviour {
+
+	public GameObject Slug;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("spawn", 1, 1);
+	}
+
+	void spawn () {
+		GameObject s = Instantiate(Slug, new Vector3(Random.Range(-35,35), 15, Random.Range(-35,35)), Quaternion.identity) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
