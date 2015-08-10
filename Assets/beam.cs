2fd590e1614e7181b3c0cf7a46a702f0321f9251
Aca7 <hoxz12012@naver.com>
2015-08-10 09:11:31 +0900

    sex

--- Assets/beam.cs
+++ Assets/beam.cs
@@ -7,23 +7,55 @@ public class beam : MonoBehaviour {
 	LineRenderer line;
 	CardboardHead head;
 	public Text gui;
+	public Text score;
 	AudioSource playerAudio;
+	bool nowPlaying = false;
+	int blinks = 0;
+	Color blinkColor;
+	Color ScoreColor;
 
-	public GameObject slug;
+	//public GameObject slug;
 	// Use this for initialization
 	void Start () {
 		Cardboard.SDK.TapIsTrigger = true;
-		print ("init");
+		//print ("init");
 		line = GetComponent<LineRenderer>();
 		line.enabled = false;
 		head = Camera.main.GetComponent<StereoController>().Head;
 		line.material = new Material (Shader.Find("Particles/Additive"));
 		playerAudio = GetComponent <AudioSource> ();
 		//slug = GameObject.Find ("Slug").transform;
+		blinkColor.r = 255;
+		blinkColor.g = 0;
+		blinkColor.b = 0;
+		blinkColor.a = 1;
+
+		ScoreColor.r = 20;
+		ScoreColor.g = 20;
+		ScoreColor.b = 20;
+		ScoreColor.a = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+				if (nowPlaying == false) {
+						blinks++;
+						score.GetComponent<Text> ().color = ScoreColor;
+						if (blinks / 40 % 2 == 1)
+								blinkColor.a = 0;
+						else
+								blinkColor.a = 1;
+
+						gui.GetComponent<Text> ().color = blinkColor;
+						Debug.Log (blinks);
+				}
+		else
+		blinkColor.a = 0;
+		ScoreColor.a = 1;
+		gui.GetComponent<Text>().color = blinkColor;
+		score.GetComponent<Text> ().color = ScoreColor;
+		
+
 		if (Cardboard.SDK.CardboardTriggered) {
 			if(GameObject.Find("GameManager").GetComponent<global>().gameStarted) {
 				StopCoroutine("fire");
@@ -31,6 +63,7 @@ public class beam : MonoBehaviour {
 				playerAudio.Play();
 			} else {
 				GameObject.Find("GameManager").GetComponent<global>().gameStarted = true;
+				nowPlaying = true;
 				print("gameStart");
 			}
 		}
@@ -49,7 +82,7 @@ public class beam : MonoBehaviour {
 							GameObject.Find("GameManager").GetComponent<global>().score += 1;			
 							Debug.Log(GameObject.Find("GameManager").GetComponent<global>().score);
 							Destroy(hit.collider.gameObject);
-				gui.GetComponent<Text>().text=GameObject.Find("GameManager").GetComponent<global>().score+"";
+				score.GetComponent<Text>().text = "Score : " + GameObject.Find("GameManager").GetComponent<global>().score;
 						}
 				}
 	
